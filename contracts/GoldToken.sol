6dfaae19e1056708ccf4cdd3c948fe795ffb2d6e
Lawrence Deacon <lawrence.deacon@gmail.com>
2020-05-04 14:53:19 +0100

    Basic ERC20 contract based on https://github.com/commerceblock/cbt-smart-contracts/tree/master/contracts

---
+++ contracts/GoldToken.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.6.6;
+
+
+import './StandardToken.sol';
+
+
+/**
+ * @title GoldToken (DGLD) ERC20 Token
+ */
+contract GoldToken is StandardToken {
+
+    string public name = "Digital Gold";
+    string public symbol = "DGLD";
+    uint256 public decimals = 8;
+
+    //uint256 public supplyExponent = 0;
+    //uint256 public totalSupply = (10 ** supplyExponent) * (10 ** decimals);
+
+    uint256 public totalSupply = 0;
+
+    function CommerceBlockToken(address pegin) {
+      balances[pegin] = totalSupply;
+    }
+
+}
