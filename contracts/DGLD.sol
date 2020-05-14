0d640106c550f8f07275d92009e253b97ed9dcfc
Lawrence Deacon <lawrence.deacon@gmail.com>
2020-05-14 14:46:00 +0100

    Update ccomments

--- contracts/DGLD.sol
+++ contracts/DGLD.sol
@@ -4,6 +4,9 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 
+/// @title wrapped-DGLD
+/// @author CommerceBlock
+/// @notice This is the contract for the wrapped-DGLD token.
 contract DGLD is ERC20Detailed, ERC20Mintable {
 
   //Tokens * decimals
@@ -20,7 +23,7 @@ contract DGLD is ERC20Detailed, ERC20Mintable {
 
 
   /**	
-     * @dev Returns the initial supply of tokens.
+     * @notice A function to get the initial supply of tokens.
     */
   function initialSupply() public pure returns (uint256)
   {
@@ -29,21 +32,22 @@ contract DGLD is ERC20Detailed, ERC20Mintable {
 
 
   /**	
-     * @dev Returns the pegout address.
+     * @notice A function to get the pegout address.
+     * 
      * ***WARNING*** any tokens transferred to this address will be destroyed.
      *
      * This address can be used to transfer DGLD tokens from this ethereum contract to the
      * DGLD federated sidechain. Failure to follow the procedure outlined below will result
-     * in permanent loss of tokens.
+     * in your tokens being destroyed. Your tokens may then be reminted and returned to you
+     * (minus a fee) but this is not guaranteed.
      *
      * For whitelisting instructions see dgld.ch.
-     * For support contact XXXXXX
      *
      * PEGOUT INSTRUCTIONS
-     * To transfer tokens to an address on the DGLD side chain:
+     * To transfer wrapped-DGLD tokens to an address on the DGLD side chain:
      * 1) Obtain a whitelisted DGLD sidechain address (`receiver` address).;
      * 2) Import the `receiver` address private key into an Ethereum wallet in order to generate the corresponding Ethereum account (`sender` account).
-     * 3) Transfer the required number of DGLD tokens to the `sender` account.
+     * 3) Transfer the required number of wrapped-DGLD tokens to the `sender` account.
      * 4) Obtain the `pegout` address using the pegoutAddress() function.
      * 5) Transfer the required number of tokens from the `sender` account to the `pegout` address.
      *
@@ -51,8 +55,10 @@ contract DGLD is ERC20Detailed, ERC20Mintable {
      * ALTERNATIVE PEGOUT INSTRUCTIONS
      * To transfer tokens to an address on the DGLD side chain:
      * 1) Import the private key from an Ethereum account (`sender` account) into a DGLD sidechain wallet and ensure the corresponding address is whitelisted (see dgld.ch for instructions on whitelisting).
-     * 2) Obtain the `pegout` address using the pegoutAddress() function.
-     * 3) From the `sender` account, transfer the required number of tokens to the `pegout` address.
+     * 2) Transfer the required number of wrapped-DGLD tokens to the `sender` account.
+     * 3) Obtain the `pegout` address using the pegoutAddress() function.
+     * 4) Transfer the required number of tokens from the `sender` account to the `pegout` address.
+     *
      *  
     */
   function pegoutAddress() public pure returns (address) {
