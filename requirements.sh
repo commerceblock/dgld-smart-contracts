2a59fe16d78137581a49e5539d42286b6b1be932
Lawrence Deacon <lawrence.deacon@gmail.com>
2020-06-29 11:26:20 +0100

    Include Migrations.sol. Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 npm install -g truffle@5.1.24
 npm install openzeppelin-solidity@2.5.0
-npm install --save-dev @openzeppelin/test-environment@2.5.0
 npm install --save-dev mocha chai
-npm install @truffle/hdwallet-provider
-npm install --save dotenv
+npm install @truffle/hdwallet-provider@1.0.36
+npm install --save dotenv@8.2.0
+npm install chai@4.2.0
+npm install mocha@8.0.1
