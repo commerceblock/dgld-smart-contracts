a0cd363def51b125a1af900f60ca4f67ff6b29af
Lawrence Deacon <lawrence.deacon@gmail.com>
2020-06-29 16:56:22 +0100

    truffle and truffle-hdwallet-provider versions updated

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-npm install -g truffle@5.1.24
+npm install -g truffle@5.1.32
 npm install openzeppelin-solidity@2.5.0
-npm install --save-dev mocha chai
-npm install @truffle/hdwallet-provider@1.0.36
+npm install --save-dev mocha@8.0.1 chai@4.2.0
+npm install truffle-hdwallet-provider@1.0.17
 npm install --save dotenv@8.2.0
 npm install chai@4.2.0
 npm install mocha@8.0.1
